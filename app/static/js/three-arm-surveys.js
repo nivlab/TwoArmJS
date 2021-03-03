640a84bba7bb3d438ba187580bbba9fb0492b27d
szorowi1 <szorowi1@gmail.com>
2021-03-02 23:34:19 -0500

    demo branch

--- app/static/js/three-arm-surveys.js
+++
@@ -1,9 +0,0 @@
-//------------------------------------//
-// Define questionnaires.
-//------------------------------------//
-
-// Demographics questionnaire
-var demographics = {
-  type: 'survey-demo',
-  data: {survey: 'demographics'}
-};
